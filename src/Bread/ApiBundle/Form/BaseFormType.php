c10d8dde73e4d2451279ab46df43cca662db8e2d
dborodin <DimonVelikii-1992@mail.ru>
2018-02-11 18:51:02 +0500

    base form type

---
+++ src/Bread/ApiBundle/Form/BaseFormType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Bread\ApiBundle\Form;
+
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+
+/**
+ * Class BaseFormType
+ * @package Bread\ApiBundle\Form
+ */
+class BaseFormType extends FormService
+{
+    /**
+     * @param FormBuilder $builder
+     * @return void
+     */
+    public function createForm(FormBuilder $builder)
+    {
+        $builder
+            ->add('Name', [
+                'constraints'   =>  [
+                    new NotBlank(['message' => 'Введите ФИО'])
+                ]
+            ])
+            ->add('Phone', [
+                'constraints'   =>  [
+                    new NotBlank(['message' => 'Введите телефон']),
+                    new Regex([
+                        'pattern' => '/^\d{10}$/',
+                        'message' => 'Неверно введен телефон'
+                    ])
+                ]
+            ])
+            ->add('Email', [
+                'constraints'   =>  [
+                    new NotBlank(['message' =>  'Введите E-mail']),
+                    new Email(['message'    =>  'E-mail введен неверно'])
+                ]
+            ])
+            ->add('Agree', [
+                'constraints'   =>  [
+                    new NotBlank(['message' => 'Необходимо согласиться с условиями']),
+                    new IsTrue(['message' => 'Необходимо согласиться с условиями'])
+                ]
+            ]);
+    }
+}
--- src/Bread/ApiBundle/Form/FeedbackType.php
+++ src/Bread/ApiBundle/Form/FeedbackType.php
@@ -2,12 +2,7 @@
 
 namespace Bread\ApiBundle\Form;
 
-use Symfony\Component\Validator\Constraints\Email;
-use Symfony\Component\Validator\Constraints\IsTrue;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Regex;
-
-class FeedbackType extends FormService
+class FeedbackType extends BaseFormType
 {
     /**
      * @param FormBuilder $builder
@@ -15,33 +10,7 @@ class FeedbackType extends FormService
      */
     public function createForm(FormBuilder $builder)
     {
-        $builder
-            ->add('Name', [
-                'constraints'   =>  [
-                    new NotBlank(['message' => 'Введите ФИО'])
-                ]
-            ])
-            ->add('Phone', [
-                'constraints'   =>  [
-                    new NotBlank(['message' => 'Введите телефон']),
-                    new Regex([
-                        'pattern' => '/^\d{10}$/',
-                        'message' => 'Неверно введен телефон'
-                    ])
-                ]
-            ])
-            ->add('Email', [
-                'constraints'   =>  [
-                    new NotBlank(['message' =>  'Введите E-mail']),
-                    new Email(['message'    =>  'E-mail введен неверно'])
-                ]
-            ])
-            ->add('Agree', [
-                'constraints'   =>  [
-                    new NotBlank(['message' => 'Необходимо согласиться с условиями']),
-                    new IsTrue(['message' => 'Необходимо согласиться с условиями'])
-                ]
-            ]);
+        parent::createForm($builder);
 
         $data = $builder->createForm();
         $data->add('Comment');
